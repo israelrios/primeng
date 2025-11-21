8db903bc61491f0a0f8af46f6ab40eeccf54c85a
Israel Rios <israel.rios@serpro.gov.br>
2025-11-20 23:10:41 -0300

    Increasing stroke width in CheckIcon component.

--- packages/primeng/src/icons/check/check.ts
+++ packages/primeng/src/icons/check/check.ts
@@ -4,7 +4,7 @@ import { BaseIcon } from 'primeng/icons/baseicon';
 @Component({
     selector: '[data-p-icon="check"]',
     standalone: true,
-    template: ` <svg:path d="M1 6 L5 10 L13 1" fill="none" stroke="#1A73E8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /> `
+    template: ` <svg:path d="M1 6 L5 10 L13 1" fill="none" stroke="#1A73E8" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" /> `
 })
 export class CheckIcon extends BaseIcon {
     viewBox = '0 0 14 11';
